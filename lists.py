863831bcb281cb9a38f4142af8733ca623e3b1ec
ikhalilali <ikhalilali@gmail.com>
2024-02-25 16:47:17 +0100

    Add lists.py

---
+++ lists.py
@@ -0,0 +1,8 @@
+# Define a list of names
+names = ['Halilu', 'Mustafa', 'Madina', 'Zainab', 'Maryam']
+# The .append function adds a value to an existing list
+names.append("Mummy")
+
+names.sort()
+
+print(names)
